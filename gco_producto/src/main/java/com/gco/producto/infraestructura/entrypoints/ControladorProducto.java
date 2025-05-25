54e2ad70ea7d54879d9bd47602161b40865b08a2
harrison felix alvarez laverde <harrisonfa200@outlook.com>
2025-05-24 20:10:15 -0500

    se agrega cross origin para llamar metodos http

--- gco_producto/src/main/java/com/gco/producto/infraestructura/entrypoints/ControladorProducto.java
+++ gco_producto/src/main/java/com/gco/producto/infraestructura/entrypoints/ControladorProducto.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/producto")
+@CrossOrigin("*")
 public class ControladorProducto {
     private final ManejadorObtenerProductos
             manejadorObtenerProductosa;
